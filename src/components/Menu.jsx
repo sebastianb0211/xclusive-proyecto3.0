63498e042ba20eecb96f4ec276feb9c8dc93a994
Sebastianb <sebastianb.0211@gmail.com>
2023-12-10 22:13:54 -0500

    se crea y se muestra la base de datos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,35 @@
+import axios from "axios";
+import Header from "../helpers/Header";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Menu = () => {
+  const [productos, setProductos] = useState([]);
+  const mostrarProductos = async () => {
+    let listadoProductos = await axios.get("http://localhost:3001/productos");
+    setProductos(listadoProductos.data);
+  };
+
   return (
     <div className="contenedor-main">
+      <button onClick={mostrarProductos}>Empezar</button>
       <div id="contenedor-productos" className="contenedor-productos">
-      <img className="producto-imagen" src="src/assets/img/logo-pagina (2).png" />
-        <div className="producto-detalles">
-          <h3 className="producto-titulo"></h3>
-          <p className="producto-precio"></p>
-          <button className="producto-agregar">
-            Agregar
-          </button>
-        </div>
+        {productos.map((producto) => (
+          <section>
+            <img
+              className="producto-imagen"
+              src={producto.imagen}
+             
+            />
+            <div class="producto-detalles">
+              <h3 class="producto-titulo">{producto.titulo}</h3>
+              <p class="producto-precio">${producto.precio}</p>
+              <button class="producto-agregar" >
+                Agregar
+              </button>
+            </div>
+          </section>
+        ))}
       </div>
     </div>
   );
